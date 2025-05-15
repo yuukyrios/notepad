0ccb2575ba1fd7c85c3f30f91c8b9254cc14714f
atha <amhanime147@gmail.com>
2025-05-15 14:19:01 +0700

    nav fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 function Navbar({ searchQuery, handleSearch }) {
   return (
-    <div className="bg-blue-600 text-white p-4 ">
+    <div className="bg-white shadow-md p-4 mb-24">
       <div className="max-w-3xl mx-auto flex items-center justify-between">
-        <h1 className="text-2xl font-bold">KodeNote</h1>
+        <img 
+          src="./logo.svg"
+          alt="KodeNote Logo"
+          className="w-32 h-auto"
+        />
         <input
           type="text"
-          placeholder="Search notes by title"
+          placeholder="Search..."
           value={searchQuery}
           onChange={handleSearch}
-          className="px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+          className="px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 w-80 border border-gray-300"
         />
       </div>
     </div>
